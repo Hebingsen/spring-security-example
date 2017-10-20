b4c3bbf453f49a855007d613b931fb5f64b551a7
hebingsen <hebs1111@13322.com>
2017-10-20 19:01:30 +0800

    cros配置

--- src/main/java/com/sky/config/MvcConfig.java
+++ src/main/java/com/sky/config/MvcConfig.java
@@ -1,19 +1,31 @@
 package com.sky.config;
 
-
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
-	
+
+	/**
+	 * 视图配置
+	 */
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/home").setViewName("home");
 		registry.addRedirectViewController("/", "/swagger-ui.html");
 		registry.addViewController("/auth/hello").setViewName("hello");
 		registry.addViewController("/login").setViewName("login");
+	}
+	
+	/**
+	 * 跨域配置
+	 */
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+				.allowCredentials(false).maxAge(3600);
 	}
 
 }
